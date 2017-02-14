37e9cdc1a5f25c4e9604299f40328c036ecb9349
Quan To <btquanto@gmail.com>
2017-02-14 11:12:27 +0700

    add flask script alchemy command

--- flaskscript.py
+++ flaskscript.py
@@ -3,6 +3,7 @@
 from flask import Flask
 from flask_script import Manager, prompt_bool
 from flask_migrate import Migrate, MigrateCommand
+from flask_alchemydumps import AlchemyDumps, AlchemyDumpsCommand
 
 from core import create_application, db
 
@@ -11,9 +12,11 @@ from models import *
 app = create_application(__name__)
 
 migrate = Migrate(app, db)
+alchemy_dumps = AlchemyDumps(app, db)
 manager = Manager(app)
 
 manager.add_command('db', MigrateCommand)
+manager.add_command('alchemy', AlchemyDumpsCommand)
 
 if __name__ == '__main__':
     manager.run()
