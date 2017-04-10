02bab253962dd71d728ef1a2d93e82554e9c7db2
Quan To <btquanto@gmail.com>
2017-04-10 12:05:23 +0700

    Add sample blueprint route /login

--- blueprints/home.py
+++ blueprints/home.py
@@ -7,3 +7,7 @@ node = Blueprint("home", __name__, template_folder="templates")
 @node.route("/index")
 def index():
     return "Hello World!!!"
+
+@node.route('/login', methods=['GET', 'POST'])
+def login():
+    return "Login"
